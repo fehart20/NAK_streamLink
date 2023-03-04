ad04186f4cdee07e31ea7b3abeca70ef0a4ed5b9
FH-Development <felix.hartnagel@web.de>
2023-03-04 11:35:35 +0100

    Added install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade -y
+sudo apt install python3 -y
+sudo apt install vlc -y
+sudo apt install cec-utils -y
+
+pip install youtube_dl
+pip install python-vlc
+pip install tk
+pip install keyboard
+
+sudo chmod ug+x ./streamLink/start.sh
+
+sh ./start.sh
